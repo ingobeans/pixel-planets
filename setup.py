a3833baad3fbaea1ed3a9f2afefc3ccacb946afb
ingobeans <127751491+ingobeans@users.noreply.github.com>
2024-03-23 23:04:53 +0100

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='pixelplanets',
-    version='0.1',
+    version='0.1.1',
     install_requires=[
         "pillow>=10.2.0"
-    ]
+    ],
     packages=find_packages(),
     exclude_package_data={'': ['example.py']},
 )
